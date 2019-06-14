1e85d899f7b607dd43cce71fc7113a817d13b2ed
gitvijayy <7822vijay@gmail.com>
2019-06-14 04:40:53 -0700

    layout test

--- app.js
+++ app.js
@@ -1,7 +1,72 @@
 $(document).ready(function () {
   console.log("in1")
-  $(document).on(`click`, `.projects`, function () {
+  $(document).on(`click`, `#education`, function () {
 
-    $('.projects-block').css("display", "none")
+    //$('.education').css("display", "flex")
+    $('.summary').css("display", "none")
+    $('.career').css("display", "none")
+    $('.projects').css("display", "none")
+    $('.skills').css("display", "flex")
+    $('#education').addClass("btn-dark")
+    $('#about').removeClass("btn-dark")
+    $('#career').removeClass("btn-dark")
+    $('#projects').removeClass("btn-dark")
+
+    $(".education").slideDown("slow", function () {
+      // Animation complete.
+    });
+
+
   })
+
+  $(document).on(`click`, `#about`, function () {
+
+    $(".summary").slideDown("slow")
+    $('.education').css("display", "none")
+    $('.skills').css("display", "flex")
+
+    $('.career').css("display", "none")
+    $('.projects').css("display", "none")
+
+    $('#education').removeClass("btn-dark")
+    $('#about').addClass("btn-dark")
+    $('#career').removeClass("btn-dark")
+    $('#projects').removeClass("btn-dark")
+  })
+
+  $(document).on(`click`, `#career`, function () {
+
+    $('.education').css("display", "none")
+    $('.summary').css("display", "none")
+    // $('.career').css("display", "flex")
+    $('.career').slideDown('slow')
+    $('.projects').css("display", "none")
+    $('.skills').css("display", "none")
+    $('#education').removeClass("btn-dark")
+    $('#about').removeClass("btn-dark")
+    $('#career').addClass("btn-dark")
+    $('#projects').removeClass("btn-dark")
+
+
+  })
+
+  $(document).on(`click`, `#projects`, function () {
+
+    $('.education').css("display", "none")
+    $('.summary').css("display", "flex")
+    $('.career').css("display", "none")
+    $('.projects').css("display", "none")
+
+    $('#education').removeClass("btn-dark")
+    $('#about').removeClass("btn-dark")
+    $('#career').removeClass("btn-dark")
+    $('#projects').addClass("btn-dark")
+
+  })
+
+
+
+
+
+
 })
